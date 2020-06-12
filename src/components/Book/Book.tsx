e2f79dc3153ed19102eccf80d8d5fcc155908ca5
nasikshafeek <nasik.shafeek@abstraction.co>
2020-06-12 22:22:32 +0530

    Starting off with book component

---
+++ src/components/Book/Book.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Book() {
+    return (
+        <div className="Book">
+            <img src="https://via.placeholder.com/250" alt=""/>
+            <h3>Some book title</h3>
+        </div>
+    );
+}
+
+export default Book;
