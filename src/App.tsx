83c40c01df3e636f4b0db233f47a6a70b47e94e3
nasikshafeek <nasik.shafeek@abstraction.co>
2020-06-19 10:17:56 +0530

    Functional component with paramater passing

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,10 @@ import './App.css';
 import Book from "./components/Book/Book";
 
 function App() {
+    const description = "Hello there is a nice text"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Book></Book>
+      <Book description={description}></Book>
     </div>
   );
 }
--- src/components/Book/Book.tsx
+++ src/components/Book/Book.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Book() {
+function Book(params: any) {
     return (
         <div className="Book">
             <img src="https://via.placeholder.com/250" alt=""/>
             <h3>Some book title</h3>
+            <h5>{params.description}</h5>
         </div>
     );
 }
